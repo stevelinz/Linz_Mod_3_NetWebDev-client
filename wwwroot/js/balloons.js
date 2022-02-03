813e7a0ce25a663b2d5e864a0824ee88a9fd3d5d
steve Linz <steve.linz@gmail.com>
2022-02-02 22:32:17 -0600

    first commit

---
+++ wwwroot/js/balloons.js
@@ -0,0 +1,17 @@
+﻿$(function(){
+    $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+      // event listener for check/uncheck
+      $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
+    });
+});
---
+++ wwwroot/js/client.js
@@ -0,0 +1,24 @@
+﻿$(function(){
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
+    $('.code').on('click', function(e) {
+        e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        // play audio
+        toast.play();
+        // display product data
+        $('#product').html($(this).data('product'));
+        $('#code').html($(this).data('code'));
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
+
+    $(document).on('keyup', function(e) {
+        if (e.key === "Escape") {
+            $('#toast').toast('hide');
+        }
+    });
+});
