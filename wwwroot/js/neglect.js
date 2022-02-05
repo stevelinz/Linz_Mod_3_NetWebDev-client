d9ae2e51bcb1fc9cc31555e53c645942e0c507b8
steve Linz <steve.linz@gmail.com>
2022-02-04 19:50:41 -0600

    Added Toast to Balloon html

---
+++ wwwroot/js/neglect.js
@@ -0,0 +1,32 @@
+$(function(){
+    $('.btn').on('click', function(e) {
+        $('#toast').toast('dispose');
+        e.preventDefault();
+        {
+            var count = 0;
+            const r = document.querySelector('#red');
+            const g = document.querySelector('#green');
+            const b = document.querySelector('#blue');
+        
+            if(r.checked || g.checked || b.checked ) count = 1;
+
+           if (count === 0)
+            {
+                $('#toast').toast({delay: 15000});
+                $('#toast').toast({ autohide: false }).toast('show');   
+            }           
+        }
+
+    });
+
+    $(document).on('keyup', function(e) {
+        if (e.key === "Escape") {
+            $('#toast').toast('hide');
+        }
+                     
+    });   
+
+});
+
+
+
